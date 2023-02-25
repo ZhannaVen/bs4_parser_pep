b965da02b588c4b155eaa08a73c75072008ce90f
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-02-25 12:51:11 +0300

    constants updated

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 
+
+#URL's
 MAIN_DOC_URL = 'https://docs.python.org/3/'
-BASE_DIR = Path(__file__).parent
-DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 PEP_DOC_URL = 'https://peps.python.org/'
+
+#Local Directories
+BASE_DIR = Path(__file__).parent
+LOG_DIR = BASE_DIR / 'logs'
+LOG_FILE = LOG_DIR / 'parser.log'
+
+#Other
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 EXPECTED_STATUS = {
         'A': ['Accepted', 'Active'],
         'D': ['Deferred'],
