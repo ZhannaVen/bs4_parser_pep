ff8080e01ec8a9937d5e0fdebebc8eba8329bc43
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-02-26 13:25:59 +0300

    outputs updated

--- src/constants.py
+++ src/constants.py
@@ -1,21 +1,25 @@
 from pathlib import Path
 
 
-#URL's
+# URL's
 MAIN_DOC_URL = 'https://docs.python.org/3/'
 PEP_DOC_URL = 'https://peps.python.org/'
 
-#Local Directories
+# Local Directories
 BASE_DIR = Path(__file__).parent
 LOG_DIR = BASE_DIR / 'logs'
 LOG_FILE = LOG_DIR / 'parser.log'
+RESULTS_DIR = BASE_DIR / 'results'
 
-#Formats
+# Formats
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
 DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 
-#Other
+# Phrases
+FILE_SAVED = 'Файл с результатами был сохранён: {}'
+
+# Other
 PRETTY = 'pretty'
 FILE = 'file'
 EXPECTED_STATUS = {
--- src/outputs.py
+++ src/outputs.py
@@ -4,25 +4,24 @@ import logging
 
 from prettytable import PrettyTable
 
-from constants import BASE_DIR, DATETIME_FORMAT
+from constants import DATETIME_FORMAT, PRETTY, FILE, RESULTS_DIR, FILE_SAVED
 
 
 def control_output(results, cli_args):
-    output = cli_args.output
-    if output == 'pretty':
-        pretty_output(results)
-    elif output == 'file':
-        file_output(results, cli_args)
-    else:
-        default_output(results)
+    outputs = {
+        FILE: file_output,
+        PRETTY: pretty_output,
+        None: default_output
+    }
+    outputs[cli_args.output](results, cli_args)
 
 
-def default_output(results):
+def default_output(results, *args):
     for row in results:
         print(*row)
 
 
-def pretty_output(results):
+def pretty_output(results, *args):
     table = PrettyTable()
     table.field_names = results[0]
     table.align = 'l'
@@ -31,7 +30,7 @@ def pretty_output(results):
 
 
 def file_output(results, cli_args):
-    results_dir = BASE_DIR / 'results'
+    results_dir = RESULTS_DIR
     results_dir.mkdir(exist_ok=True)
     parser_mode = cli_args.mode
     now = dt.datetime.now()
@@ -41,4 +40,4 @@ def file_output(results, cli_args):
     with open(file_path, 'w', encoding='utf-8') as f:
         writer = csv.writer(f, dialect='unix')
         writer.writerows(results)
-    logging.info(f'Файл с результатами был сохранён: {file_path}')
+    logging.info(FILE_SAVED.format(file_path))
